0e06574f10c432ffe6abc9bc3b9bf45d47c9874b
shaileshprusty <shailesh.prusty@gmail.com>
2018-05-02 10:02:29 +0530

    Pattern printing

---
+++ c-files/Sikun/loop.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+void main()
+{
+		// int a=0;
+		// printf("Give me a no:");
+		// scanf("%d",&a);
+      
+		// while(a<100)
+		// {
+		// 	printf("happy birthday\n");
+		// 	a=a+10;
+		// }
+  //      printf("loops terminated....!!!!!");
+// 	int x, n;
+// 	printf("Give me two numbers:");
+// 	scanf("%d%d",&a,&b);
+// 	printf("%d", 1-x)
+
+	int a=0;
+	while(a<7)
+	{
+		printf("&");
+		a=a+1;
+	}
+	printf("\n");
+	a=0;
+    	while(a<5)
+    	{
+    		printf("&");
+    		a=a+1;
+    	}
+	printf("\n");
+	a=0;
+    	while(a<3)
+    	{
+    		printf("&");
+    		a=a+1;
+    	}
+	printf("\n");
+	a=0;
+    	while(a<1)
+    	{
+    		printf("&");
+    		a=a+1;
+    	}
+	printf("\n");
+ }

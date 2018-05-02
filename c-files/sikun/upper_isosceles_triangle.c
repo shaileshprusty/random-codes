1869d5460df28a656d0ca6eca37b95868726abe3
shaileshprusty <shailesh.prusty@gmail.com>
2018-05-02 20:42:14 +0530

    upper isosceles triangle

---
+++ c-files/sikun/upper_isosceles_triangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int i, j, k;
+        printf("No of lines: ");
+        scanf("%d", &i);
+        for(j=i; j>0; --j)
+	{
+		for(k=0; k<i-j; ++k)
+			printf("  ");
+		for(k; k<j; ++k)
+			printf("& ");
+		printf("\n");
+	}
+}
